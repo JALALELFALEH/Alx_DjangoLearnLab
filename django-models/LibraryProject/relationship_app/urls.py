50de02922283c998b81c6ab3604756070e663358
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-11-19 01:52:04 +0100

    2. Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView, register_view
+from django.contrib.auth import views as v
+from . import views
 
 
 urlpatterns = [
-    path('', list_books, name='book-list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path('login/' , auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html')),
-    path('register/', register_view, name='register'),
+    path('', views.list_books, name='book-list'),
+    path('library/<int:pk>/', viewsLibraryDetailView.as_view(), name='library-detail'),
+    path('login/' , v.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', v.LogoutView.as_view(template_name='relationship_app/logout.html')),
+    path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,7 +18,7 @@ class LibraryDetailView(DetailView):
     context_object_name = 'library'
     template_name = 'relationship_app/library_detail.html'
 
-def register_view(request):
+def register(request):
     if request.method == 'POST' :
         form = UserCreationForm(request.POST)
         if form.is_valid():
