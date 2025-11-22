dd5c86ef7cf82919a8cc2f7443ca482c1cb739b6
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-11-22 17:28:37 +0100

    0. Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Book
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 from django.utils.translation import gettext_lazy as _
-# Register your models here.
+from .models import Book, CustomUser
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -11,11 +9,11 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
 
+
 admin.site.register(Book, BookAdmin)
 
-@admin.register(CustomUser)
+
 class CustomUserAdmin(UserAdmin):
-    # Add the extra fields to admin forms and list display
     fieldsets = UserAdmin.fieldsets + (
         (_('Extra info'), {'fields': ('date_of_birth', 'profile_photo')}),
     )
@@ -24,4 +22,13 @@ class CustomUserAdmin(UserAdmin):
         (_('Extra info'), {'fields': ('date_of_birth', 'profile_photo')}),
     )
 
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'date_of_birth'
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
