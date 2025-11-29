94b2f3f4c17e08c1de2461265c4bc587d18fa2d9
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-11-29 22:31:14 +0100

    3. Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookViewSet
 
 router = DefaultRouter()
 router.register(r'book_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('auth-token/', obtain_auth_token, name='api-token-auth'),
 ]
