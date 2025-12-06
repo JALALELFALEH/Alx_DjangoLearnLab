2f6e06182c4e3f6fd77b3e4e41b0f6ce1e5c5b23
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-06 18:41:16 +0100

    1. Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/advanced_api_project/wsgi.py
+++ advanced-api-project/advanced_api_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced-api-projec.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced_api_project.settings')
 
 application = get_wsgi_application()
