916ba06de1ed37b3b6aa1ccf2c8d6881e242999b
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-07 23:30:24 +0100

    2. Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,11 +14,11 @@ urlpatterns = [
     # Home page - shows all posts
     path('', PostListView.as_view(), name='home'),
     
-    # Post CRUD URLs
-    path('posts/new/', PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    # Post CRUD URLs - Fixed according to task requirements
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     
     # Authentication URLs
     path('register/', views.register, name='register'),
@@ -42,4 +42,4 @@ urlpatterns = [
     path('password-reset-complete/', 
             auth_views.PasswordResetCompleteView.as_view(template_name='blog/password_reset_complete.html'), 
             name='password_reset_complete'),
-    ]
+]
