c7eb2fbb32069552a46fb93ad96afb3f345ce936
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-13 15:15:20 +0100

    0. Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -28,7 +28,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
     
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
 
